1ece3ffba86311d8091c8664569b0f382f195a27
Ellomend <mrsotona@gmail.com>
2022-11-28 23:59:03 +0300

    outlines

--- src/components/models.ts
+++ src/components/models.ts
@@ -6,3 +6,24 @@ export interface Todo {
 export interface Meta {
   totalCount: number;
 }
+
+export interface CheckListDto {
+  id: string;
+  name: string;
+  tasks: TaskDto[];
+  user: UserDto;
+}
+
+export interface TaskDto {
+  id: string;
+  name: string;
+  description: string;
+  completed: boolean;
+  order: number;
+}
+
+export interface UserDto {
+  id: string;
+  email: string;
+  password?: string;
+}
